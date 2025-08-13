9f9c811f00146a06c3fa29f18184d6b800043ae3
Sammking120 <sammking120@gmail.com>
2025-08-13 19:42:11 +0300

    Debug on

--- alx_movierec_backend/settings.py
+++ alx_movierec_backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'djangoDEV-secret-key')
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = 'True'
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
