020251e52051cdd01202c77cf0eae94541a484a2
Sammking120 <sammking120@gmail.com>
2025-08-13 19:51:39 +0300

    Testing

--- alx_movierec_backend/settings.py
+++ alx_movierec_backend/settings.py
@@ -31,7 +31,7 @@ TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'True'
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 # Static files settings
 STATIC_URL = '/static/'
